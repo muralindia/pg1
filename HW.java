5d6ce085d2ac935fad2b17265cedfbe6b2e645d0
muralindia <muralindia@gmail.com>
2018-06-24 02:56:24 +0530

    Update and rename hw.java to HW.java

--- HW.java
+++ HW.java
@@ -1,6 +1,6 @@
-public class HWd {
+public class HW {
    public static void main(String[] args) {
       // Prints "2nd File" in the terminal window.
-      System.out.println("H, Wd");
+      System.out.println("H, W");
    }
 }
